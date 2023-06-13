2755ff11989be1429467f7340e81c43aca23d8b0
Dev-ashank <kumardivyashankgaurav@gmail.com>
2023-06-13 12:06:44 +0530

    isAnagram Done

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -8,7 +8,20 @@
 */
 
 function isAnagram(str1, str2) {
+  if (str1.length !== str2.length) {
+    return false;
+  }
+  const mySet = new Set();
+  for (let i = 0; i < str1.length; i++) {
+    mySet.add(str1[i].toLowerCase());
+  }
+  for (let i = 0; i < str2.length; i++) {
+    if (!mySet.has(str2[i].toLowerCase())) {
+      return false;
+    }
 
+    return true;
+  }
 }
 
 module.exports = isAnagram;
