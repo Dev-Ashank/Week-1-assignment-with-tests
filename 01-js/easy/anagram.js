ac91d155e37b8ea72c8739a04734b545dbe76f98
Dev-ashank <kumardivyashankgaurav@gmail.com>
2023-06-14 14:23:24 +0530

    error fixed update in code

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -8,20 +8,11 @@
 */
 
 function isAnagram(str1, str2) {
-  if (str1.length !== str2.length) {
-    return false;
-  }
-  const mySet = new Set();
-  for (let i = 0; i < str1.length; i++) {
-    mySet.add(str1[i].toLowerCase());
-  }
-  for (let i = 0; i < str2.length; i++) {
-    if (!mySet.has(str2[i].toLowerCase())) {
-      return false;
-    }
-
-    return true;
-  }
+  if (str1.length !== str2.length) return false;
+  let sort1 = str1.toLowerCase().split("").sort().join("");
+  let sort2 = str2.toLowerCase().split("").sort().join("");
+  if (sort1 === sort2) return true;
+  return false;
 }
 
 module.exports = isAnagram;
